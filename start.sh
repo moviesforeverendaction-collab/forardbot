585815d8a13d02fdfdce6da84a90c2c543dee26b
moviesforeverendaction-collab <moviesforeverendaction@gmail.com>
2026-03-15 10:04:46 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,13 +1,17 @@
 echo "Cloning Repo...."
-if [ -z $BRANCH ]
+
+if [ -z "$BRANCH" ]
 then
-  echo "Cloning main branch...."
-  git clone https://github.com/JishuDeveloper/Ultra-Forward-Bot JishuDeveloper/Ultra-Forward-Bot
+echo "Cloning main branch...."
+git clone https://github.com/JishuDeveloper/Ultra-Forward-Bot
 else
-  echo "Cloning $BRANCH branch...."
-  git clone https://github.com/JishuDeveloper/Ultra-Forward-Bot -b $BRANCH /Ultra-Forward-Bot
+echo "Cloning $BRANCH branch...."
+git clone -b "$BRANCH" https://github.com/JishuDeveloper/Ultra-Forward-Bot
 fi
-cd JishuDeveloper/Ultra-Forward-Bot
+
+cd Ultra-Forward-Bot || exit
+
 pip3 install -U -r requirements.txt
+
 echo "Starting Bot...."
 python3 main.py
